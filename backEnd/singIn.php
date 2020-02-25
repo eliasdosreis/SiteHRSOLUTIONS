ee699bdec634336c52070a588e4ef55e899ce36e
eliasdosreis <elias_dos_reis@hotmail.com>
2020-02-25 09:08:51 -0300

    Adicionando Dasboard_01

--- backEnd/singIn.php
+++ backEnd/singIn.php
@@ -18,7 +18,7 @@ $row = mysqli_num_rows($result);
 
 if($row == 1){
     $_SESSION['usuario'] = $usuario;
-    header('Location: ./../index.php');
+    header('Location: ./../dashboard/index.html');
     exit();
 }else{
     $_SESSION['nao_autenticado'] = true;
