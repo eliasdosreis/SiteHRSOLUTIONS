2465768afc3b2fa29c07a00cd7b8df342af8ba11
eliasdosreis <elias_dos_reis@hotmail.com>
2020-02-29 17:52:28 -0300

    Email_11

--- index.php
+++ index.php
@@ -136,14 +136,14 @@ include("./backEnd/register.php")
       <div class="col-lg-4">
         <div class="card_ mb-5 mb-lg-0">
           <div class="card-body">
-            <h5 class="card-title text-muted text-uppercase text-center">Marketing WhatsApp</h5>
-            <h6 class="card-price text-center">R$ 20<span class="period">/mensal</span></h6>
+            <h5 class="card-title text-muted text-uppercase text-center">Estrategia MHI</h5>
+            <h6 class="card-price text-center">Free<span class="period">/mensal</span></h6>
             <hr>
             <ul class="fa-ul">
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Planilha basic</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Tela de configuraçao</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Resultado</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>------</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>------</li>
               </ul>
             <a href="#" class="btn btn-block btn-primary text-uppercase">COMPRAR</a>
           </div>
